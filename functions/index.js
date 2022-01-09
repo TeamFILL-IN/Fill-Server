cb237fdbeef5da110ff91d4862b59105059ecebe
권세훈 <hoon@gwonsehun-ui-MacBookPro.local>
2022-01-09 16:38:05 +0900

    [UPDATE] CI/CD 작업 마무리

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,9 @@ const helmet = require('helmet');
 const admin = require('firebase-admin');
 const serviceAccount = require('./fill-in-13efb-firebase-adminsdk-g93iy-d395b45dac.json');
 const dotenv = require('dotenv');
+const { fail } = require('../../lib/util');
+const sc = require('../../constants/statusCode');
+const rm = require('../../constants/responseMessage');
 
 dotenv.config();
 
@@ -19,8 +22,6 @@ if (admin.apps.length === 0) {
 } else {
   firebase = admin.app();
 }
-
-// dotenv.config();
 
 // initializing
 const app = express();
@@ -35,14 +36,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api', require('./api/routes'));
+app.use('/api', require('./api'));
 
 app.use('*', (req, res) => {
-  res.status(404).json({
-    status: 404,
-    success: false,
-    message: '잘못된 경로입니다.',
-  });
+  res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.NOT_FOUND));
 });
 
 exports.app = functions
@@ -51,26 +48,3 @@ exports.app = functions
     memory: '512MB', // 서버에 할당되는 메모리
   })
   .https.onRequest(app);
-// exports.app = functions
-//   .runWith({
-//     timeoutSeconds: 300, // 요청을 처리하는 과정이 300초를 초과하면 타임아웃 시키기
-//     memory: '512MB', // 서버에 할당되는 메모리
-//   })
-//   .region('asia-northeast3')
-//   .https.onRequest(async (req, res) => {
-//     // 들어오는 요청에 대한 로그를 콘솔에 찍기. 디버깅 때 유용하게 쓰일 예정.
-//     // TODO. 콘솔 수정하기
-//     console.log(
-//       '\n\n',
-//       '[api]',
-//       `[${req.method.toUpperCase()}]`,
-//       req.originalUrl,
-//       `[${req.method}] ${!!req.user ? `${req.user.id}` : ``} ${req.originalUrl}\n ${!!req.query && `query: ${JSON.stringify(req.query)}`} ${!!req.params && `params ${JSON.stringify(req.params)}`}`,
-//     );
-
-//     return app(req, res);
-//   });
-
-// module.exports = {
-//   api: require('./api'),
-// };
