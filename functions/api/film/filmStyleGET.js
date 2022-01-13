700b324331f7402525075caf5b06f8ed8bfb8edc
권세훈 <hoon@gwonsehun-ui-MacBookPro.local>
2022-01-13 17:56:49 +0900

    [ADD] Slack Webhook 추가

--- functions/api/film/filmStyleGET.js
+++ functions/api/film/filmStyleGET.js
@@ -4,28 +4,29 @@ const sc = require('../../constants/statusCode');
 const rm = require('../../constants/responseMessage');
 const db = require('../../db/db');
 const { filmDB } = require('../../db');
+const slack = require('../../other/slack/slack');
 
 module.exports = async (req, res) => {
-
   const { styleId } = req.params;
   if (!styleId) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
-  
+
   let client;
-  
+
   try {
     client = await db.connect(req);
 
     const filmsOfStyle = await filmDB.getFilmsByStyle(client, styleId);
-    
+
     if (!filmsOfStyle) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.INVALID_STYLE_ID));
-    
+
     res.status(sc.OK).send(success(sc.OK, rm.READ_FILMS_OF_STYLE_SUCCESS, filmsOfStyle));
   } catch (error) {
+    slack.slackWebhook(req, error.message);
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
-    
+
     res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
   } finally {
     client.release();
-  };
+  }
 };
---
+++ functions/other/slack/slackAPI.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const DEV_WEB_HOOK_ERROR_MONITORING = process.env.DEV_WEB_HOOK_ERROR_MONITORING;
+
+const sendMessageToSlack = (message, apiEndPoint = DEV_WEB_HOOK_ERROR_MONITORING) => {
+  try {
+    axios
+      .post(apiEndPoint, { text: message })
+      .then((response) => {})
+      .catch((e) => {
+        throw e;
+      });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const slackAPI = {
+  sendMessageToSlack,
+  DEV_WEB_HOOK_ERROR_MONITORING,
+};
+
+export default slackAPI;
