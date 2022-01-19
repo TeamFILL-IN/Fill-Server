1dda75ca8ab1f61406b466211a4aa4857e8eee42
권세훈 <hoon@gwonsehun-ui-MacBookPro.local>
2022-01-19 20:14:19 +0900

    [UPDATE] 서버 etag 해제

--- functions/index.js
+++ functions/index.js
@@ -36,6 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.disable('etag');
+app.set('etag', false);
 
 app.use('/api', require('./api'));
 
