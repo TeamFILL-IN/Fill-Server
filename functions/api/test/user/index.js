4da1175e2cc0aa078163697a2ca7a37648df2f4b
sua <72644361+sssua-0928@users.noreply.github.com>
2022-01-20 12:32:07 +0900

    [FIX] 테스트 환경 구축

--- functions/api/test/user/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { auth } = require('../../middlewares/auth');
-
-const router = express.Router();
-
-router.get('/', auth, require('./userGET'));
-router.delete('/', auth, require('./userDELETE'));
-
-module.exports = router;
