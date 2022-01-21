3a7ce0227e8a8fb2f51805ebf0a1d6413e950696
권세훈 <hoon@gwonsehun-ui-MacBookPro.local>
2022-01-21 19:02:51 +0900

    [UPDATE] header 설정

--- functions/index.js
+++ functions/index.js
@@ -26,6 +26,11 @@ if (admin.apps.length === 0) {
 
 const app = express();
 app.use(cors({ origin: true }));
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(hpp());
