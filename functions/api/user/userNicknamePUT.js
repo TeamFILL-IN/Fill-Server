ae3cb1ee0934cf96c2338dbe9f4b6d5437882285
SEHOON <devkwonsehoon@naver.com>
2022-02-16 17:06:03 +0900

    [ADD] 내 정보 수정 API 완성

---
+++ functions/api/user/userNicknamePUT.js
@@ -0,0 +1,37 @@
+const functions = require('firebase-functions');
+const { success, fail } = require('../../lib/util');
+const sc = require('../../constants/statusCode');
+const rm = require('../../constants/responseMessage');
+const db = require('../../db/db');
+const { userDB } = require('../../db');
+const { slack } = require('../../other/slack/slack');
+
+/**
+ * @닉네임_변경
+ * @desc 마이페이지에서 닉네임을 변경해요.
+ */
+module.exports = async (req, res) => {
+  const userId = req.user.id;
+  const { nickname } = req.body;
+
+  if (!userId) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
+
+  let client;
+
+  try {
+    client = await db.connect(req);
+
+    const updatedUser = await userDB.updateUserNickname(client, userId, nickname);
+    if (!updatedUser) return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.NO_USER));
+
+    res.status(sc.OK).send(success(sc.OK, rm.UPDATE_ONE_USER_SUCCESS, { updatedUser }));
+  } catch (error) {
+    slack(req, error.message);
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+
+    res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
